16b1763001c8e5bb67068767314f3bccbec10f14
Valonekiil <149128324+Valonekiil@users.noreply.github.com>
2025-03-27 15:19:30 +0700

    paralex, health, flying done

--- Actor/enemy/flying_enemy.gd
+++ Actor/enemy/flying_enemy.gd
@@ -12,7 +12,7 @@ var last_player_position: Vector2 = Vector2.ZERO
 var is_player_detected: bool = false
 # Apakah musuh sudah memberikan damage?
 var has_given_damage: bool = false
-
+@onready var texture = $Sprite2D.texture
 # Referensi ke Area2D untuk deteksi dan damage
 @onready var detection_area: Area2D = $DetectionArea
 @onready var damage_area: Area2D = $damage
@@ -51,21 +51,22 @@ func chase_player(delta):
 	move_and_slide()
 
 	# Jika musuh sudah dekat dengan posisi terakhir player
-	if global_position.distance_to(last_player_position) < 20:
-		print("sudah dekat")
-		# Cek apakah player masih di dalam DamageArea
-		var bodies = damage_area.get_overlapping_bodies()
-		for body in bodies:
-			if body.is_in_group("player"):  # Ganti "Player" dengan nama node player Anda
-				if not has_given_damage:
-					## Berikan damage ke player
-					#body.take_damage(damage)  # Pastikan player memiliki fungsi `take_damage`
-					has_given_damage = true
-					print("meledak kena player")
-					## Hapus musuh setelah memberikan damage
-					queue_free()
-		# Hentikan pengejaran
-		is_player_detected = false
+	if global_position.distance_to(last_player_position) < texture.get_width():
+		kaboom()
+
+func kaboom():
+	print("meledak")
+	# Cek apakah player masih di dalam DamageArea
+	var bodies = damage_area.get_overlapping_bodies()
+	for body in bodies:
+		if body.is_in_group("player"):  # Ganti "Player" dengan nama node player Anda
+			if not has_given_damage and body.has_node("Health_Component"):
+				## Berikan damage ke player
+				var v:Health_Component = body.get_node("Health_Component")
+				v.take_damage(damage)
+				has_given_damage = true
+				print("meledak kena player")
+	queue_free()
 
 func _on_detection_area_body_entered(body):
 	# Jika player masuk ke area deteksi dan belum terdeteksi sebelumnya
@@ -74,3 +75,8 @@ func _on_detection_area_body_entered(body):
 		last_player_position = body.global_position
 		speed = 200
 		print("Kepek musuh")
+
+
+func _on_damage_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and is_player_detected:
+		kaboom()
---
+++ hurt_box_component.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var hp:Health_Component
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
